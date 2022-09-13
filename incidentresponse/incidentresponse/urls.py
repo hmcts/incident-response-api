939ece42cc1a203778f4bdf118ee92c503dd78df
Hannah Smith <hannahs@kainos.com>
2022-09-13 18:12:51 +0100

    add readiness

--- incidentresponse/incidentresponse/urls.py
+++ incidentresponse/incidentresponse/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('slack/', include('response.slack.urls')),
     path('core/', include('response.core.urls')),
     url(r'^health(/liveness)?', include('health_check.urls')),
+    url(r'^health(/readiness)?', include('health_check.urls')),
     path('', include('response.ui.urls')),
 ]
